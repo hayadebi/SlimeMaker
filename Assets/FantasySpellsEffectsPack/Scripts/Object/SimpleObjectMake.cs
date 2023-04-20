8e0031cbbf08a0a7ee13062d89a460eda07b4e7a
hayadebi <76168948+hayadebi@users.noreply.github.com>
2023-04-20 21:51:26 +0900

    Asset upload
    Asset upload

---
+++ Assets/FantasySpellsEffectsPack/Scripts/Object/SimpleObjectMake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectMake : _ObjectMakeBase {
+
+    public Vector3 m_randomRotationValue;
+    public bool newRotTrg = true;
+    void Start(){
+        for(int i = 0; i < m_makeObjs.Length; i++)
+        { 
+            GameObject m_obj = Instantiate(m_makeObjs[i], this.transform.position, this.transform.rotation);
+            m_obj.transform.parent = this.transform;
+            if (newRotTrg)
+            {
+                m_obj.transform.rotation *= Quaternion.Euler(GetRandomVector(m_randomRotationValue));
+            }
+            if (m_movePos){ 
+                if(m_obj.GetComponent<MoveToObject>()){ 
+                    MoveToObject m_script = m_obj.GetComponent<MoveToObject>();
+                    m_script.m_movePos = m_movePos;
+                }
+            }
+        }
+    }
+}
