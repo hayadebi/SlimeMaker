6594e5d529595f8b0f5e1a4c4a671e2b0193e2ab
hayadebi <76168948+hayadebi@users.noreply.github.com>
2023-05-12 14:54:09 +0900

    2023/05/12_14:53_220038
    bomboo

---
+++ Assets/Resources/script/slime/gimmick/takegoal.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class takegoal : MonoBehaviour
+{
+    private GameObject[] tanzaku;
+    public int tanzaku_num=0;
+    public GameObject[] lv_goalsprite;
+    [System.Serializable]
+    public struct LvGoal
+    {
+        public bool[] set_active;
+    }
+    public LvGoal[] lv_goal;
+    public GameObject effect;
+    private AudioSource _audio;
+    public AudioClip se;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        Invoke(nameof(StartSet), 0.2f);
+    }
+    void StartSet()
+    {
+        tanzaku = GameObject.FindGameObjectsWithTag("bamboo");
+        tanzaku_num = tanzaku.Length;
+        SetGoal(0);
+    }
+    public void SetGoal(int addnum = -1)
+    {
+        tanzaku_num += addnum;
+        _audio.PlayOneShot(se);
+        Instantiate(effect, transform.position, transform.rotation);
+        if (tanzaku_num == tanzaku.Length)
+        {
+            for (int i = 0; i < lv_goalsprite.Length;)
+            {
+                lv_goalsprite[i].SetActive(lv_goal[0].set_active[i]);
+            }
+        }
+        else if (tanzaku_num <= 0)
+        {
+            for (int i = 0; i < lv_goalsprite.Length;)
+            {
+                lv_goalsprite[i].SetActive(lv_goal[2].set_active[i]);
+            }
+            this.gameObject.tag = "goal";
+        }
+        else if (tanzaku_num <= tanzaku.Length/2)
+        {
+            for (int i = 0; i < lv_goalsprite.Length;)
+            {
+                lv_goalsprite[i].SetActive(lv_goal[1].set_active[i]);
+            }
+        }
+    }
+}
---
+++ Assets/Resources/script/slime/gimmick/tanzaku.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tanzaku : MonoBehaviour
+{
+    public GameObject[] goals;
+    public takegoal[] takegs;
+    public int set_setrg = 0;
+    public GameObject effect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Invoke(nameof(StartSet), 0.2f);
+    }
+    void StartSet()
+    {
+        goals = GameObject.FindGameObjectsWithTag("takegoal");
+        takegs = new takegoal[goals.Length];
+        for (int i = 0; i < goals.Length;)
+        {
+            takegs[i] = goals[i].GetComponent<takegoal>();
+            i++;
+        }
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "player")
+        {
+            for (int i = 0; i < takegs.Length;)
+            {
+                takegs[i].SetGoal(-1);
+                i++;
+            }
+            GManager.instance.setrg = set_setrg;
+            Instantiate(effect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
